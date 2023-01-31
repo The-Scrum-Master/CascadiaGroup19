7df9cc0b5be4372aed8fe748a4b77f469e51f4b5
Fletcher53 <sergiojdesantiago@gmail.com>
2023-01-31 16:50:40 +0000

    added functionality into the ViewFile class to create the tiles and move them, although its not finished its in process

--- CascadiaCode/src/ViewTile.java
+++ CascadiaCode/src/ViewTile.java
@@ -1,31 +1,77 @@
+import java.util.Scanner;
+
 public class ViewTile {
-    public static void starterTiles()
+    public void starterTiles()
     {
-
-        System.out.println(DisplayColour.BLACK+"\t\t\t* * * * * * * *");
-        System.out.println("\t\t\t*\t\t\t  *");
-        System.out.println("\t\t\t*\t\t\t  *");
-        System.out.println("\t\t\t*\t\t\t  *");
-        System.out.println("\t\t\t* * * * * * * *");
+        topTileForStarterTile();
         //////////
-        System.out.print("\t* * * * * * * *\t* * * * * * * *\n");
-        System.out.print("\t*\t\t\t  *\t*\t\t\t  *\n");
-        System.out.print("\t*\t\t\t  *\t*\t\t\t  *\n");
-        System.out.print("\t*\t\t\t  *\t*\t\t\t  *\n");
-        System.out.print("\t* * * * * * * *\t* * * * * * * *");
-
-        ////////
+        System.out.print("   * * * * * * * * * * * * * * * *\n");
+        System.out.print("   *             * *             *\n");
+        System.out.print("   *             * *             *\n");
+        System.out.print("   *             * *             *\n");
+        //System.out.print(DisplayColour.RED + totalWidthForInitialTile() + DisplayColour.RESET + DisplayColour.BLUE + totalWidthForInitialTile() + DisplayColour.RESET);
+        //System.out.print(totalWidthForInitialBottomTile()+totalWidthForInitialBottomTile());
+        System.out.print("   * * * * * * * * * * * * * * * *\n");
 
     }
-    public void makeTile()
+    public void topTileForStarterTile()
     {
-
-        System.out.println("\t\t\t\t* * * * * * * *");
-        System.out.println("\t\t\t\t*\t\t\t  *");
-        System.out.println("\t\t\t\t*\t\t\t  *");
-        System.out.println("\t\t\t\t*\t\t\t  *");
-        System.out.println("\t\t\t\t* * * * * * * *");
+        System.out.println("  "+ DisplayColour.RED + totalWidthForInitialUpperTile() + DisplayColour.RESET);
+        System.out.println("           "+totalHeight());
+        System.out.println("           "+totalHeight());
+        System.out.println("           "+totalHeight());
+        System.out.println("  "+ DisplayColour.RED + totalWidthForInitialUpperTile() + DisplayColour.RESET);
     }
 
 
+
+    public String totalWidth(){
+        return "* * * * * * * *";
+    }
+    public String totalHeight(){
+        return "*             *";
+    }
+    public String totalWidthForInitialUpperTile(){
+        return "         * * * * * * * *";
+    }
+    public String totalHeightForInitialUpperTile(){
+        return "         *           *";
+    }
+    public String totalWidthForInitialBottomTile(){
+        return DisplayColour.RED + "   * * * * * * * *" + DisplayColour.RESET;
+        //return "\t* * * * * * * *";
+    }
+
+    public String makeSingleTile()
+    {
+        return totalWidth()+"\n"+totalHeight()+"\n"+totalHeight()+"\n"+totalHeight()+"\n"+totalWidth();
+        /*System.out.println(DisplayColour.BLUE + totalWidth());
+        System.out.println(totalHeight());
+        System.out.println(totalHeight());
+        System.out.println(totalHeight());
+        System.out.println(totalWidth() + DisplayColour.RESET);*/
+    }
+
+    public int whiteSpaces(){
+        System.out.println("how many white spaces?");
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
+    }
+
+    public void finalPrint(){
+        int whiteSpaces=whiteSpaces();
+
+        for(int i=0; i<whiteSpaces; i++){
+            System.out.print(" ");
+        } System.out.println(DisplayColour.BLUE + totalWidth());
+
+        for(int j=0; j<3; j++){
+            for(int i=0; i<whiteSpaces; i++){
+                System.out.print(" ");
+            } System.out.println(totalHeight());
+        }
+        for(int i=0; i<whiteSpaces; i++){
+            System.out.print(" ");
+        } System.out.println(totalWidth() + DisplayColour.RESET);
+    }
 }
