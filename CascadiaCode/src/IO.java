1feaf00dbf5da809a2bde271ad7a0a0f94435de8
Fletcher53 <sergiojdesantiago@gmail.com>
2023-01-31 13:08:17 +0000

    created the randomiser function, couple helper functions to print names and created a function to order the players

--- CascadiaCode/src/IO.java
+++ CascadiaCode/src/IO.java
@@ -1,16 +1,17 @@
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class IO {
 
     String participants;
     int participantsInt;
-    ArrayList<String> playerNames;
+    ArrayList<String> playerNames= new ArrayList<String>();
+    ArrayList<Integer> order= new ArrayList<Integer>();
+    static Scanner in =new Scanner(System.in);
 
     public static String takeInput(){
-        Scanner in =new Scanner(System.in);
-        String input=in.nextLine();
-        return makeLowerCase(input);
+        return in.nextLine();
     }
 
     public static String makeLowerCase(String str){
@@ -34,9 +35,64 @@ public class IO {
         }
     }
 
+    public void playerNames(){
+        System.out.println("Enter player names: ");
+        playerNames.add(takeInput());
+        for(int i=1; i<participantsInt; i++){
+            System.out.println("Enter next player's name: ");
+            playerNames.add(takeInput());
+        }
+        printPlayerNames();
+    }
+
+    public void playerOrder(){
+        System.out.println("The player order will be as follows: ");
+        for(int i=0; i<participantsInt; i++){
+            boolean reGenerate=false;
+            int j=randomNumberGenerator(participantsInt);
+            if(order.isEmpty()){
+                order.add(j);
+            }
+            else{
+                for(int k=0; k<order.size(); k++){
+                    if(j==order.get(k)){
+                        reGenerate=true;
+                        break;
+                    }
+                }
+                if(reGenerate){
+                    i--;
+                }
+                else{
+                    order.add(j);
+                }
+            }
+        }
+        printPlayerNamesInOrder();
+    }
+
+    public void printPlayerNames(){
+        for(int i=0; i<participantsInt; i++){
+            System.out.println(playerNames.get(i));
+        }
+    }
+
+    public void printPlayerNamesInOrder(){
+        for(int i=0; i<participantsInt; i++){
+            System.out.println(playerNames.get(order.get(i)));
+        }
+    }
+
+    public int randomNumberGenerator(int upperBound){
+        Random rand = new Random ();
+        return rand.nextInt(upperBound);
+    }
+
     public static void main(String[] args) {
         IO newGame = new IO();
         newGame.welcomeMessage();
         newGame.numberOfPlayers();
+        newGame.playerNames();
+        newGame.playerOrder();
     }
 }
