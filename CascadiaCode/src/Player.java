7148490e5315ccbebb31fab349fef71131d8886a
Patkelly17 <patrick.kelly6@ucdconnect.ie>
2023-02-06 20:07:55 +0000

    updated starter tile location

--- CascadiaCode/src/Player.java
+++ CascadiaCode/src/Player.java
@@ -8,12 +8,12 @@ public class Player {
         this.name = name;
         this.order = order;
         playerBoard= new Tile[20][20]; //new board of 20 by 20 tiles
-        playerBoard[0][1] = new Tile(Tile.tileType.SOLO,0); //generates the three starting tiles
-        playerBoard[0][1].playTile(); //changes boolean on tile to played.
-        playerBoard[0][0] = new Tile(Tile.tileType.NORMAL,2);
-        playerBoard[0][0].playTile();
-        playerBoard[1][0] = new Tile(Tile.tileType.NORMAL, 3);
-        playerBoard[1][0].playTile();
+        playerBoard[9][9] = new Tile(Tile.tileType.SOLO,0); //generates the three starting tiles
+        playerBoard[9][9].playTile(); //changes boolean on tile to played.
+        playerBoard[10][9] = new Tile(Tile.tileType.NORMAL,2);
+        playerBoard[10][9].playTile();
+        playerBoard[10][10] = new Tile(Tile.tileType.NORMAL, 3);
+        playerBoard[10][10].playTile();
     }
     public Tile[][] getPlayerBoard(){return  playerBoard;}
     public Tile getBoardIndex(int column, int row){
