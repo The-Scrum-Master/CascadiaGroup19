c55ecd39ffa8e311a9c2e2488e9391795e885236
Timisalam <102313891+Timisalam@users.noreply.github.com>
2023-03-01 10:57:42 +0000

    input/output
    got rid of next command and also handles erraneos input for inputs

--- CascadiaCode/src/GameRunner.java
+++ CascadiaCode/src/GameRunner.java
@@ -31,8 +31,8 @@ public class GameRunner {
 
                 helperIntToPrintMap++;
             }
-            else{
-                System.out.println(players.get(playersTurn).getName()+ "'s turn:");
+            else {
+                System.out.println(players.get(playersTurn).getName() + "'s turn:");
 
                 //here, if a bool hasAlreadyStarted==false, then the starter tiles should be displayed
                 //here, the 4 pairs habitats-tokens have to be displayed for a player to choose
@@ -41,28 +41,26 @@ public class GameRunner {
                 //the game will end when the players reach 20 turns
 
 
-                if(!players.get(playersTurn).isFirstTurnPlayed()) {
+                if (!players.get(playersTurn).isFirstTurnPlayed()) {
                     players.get(playersTurn).generateInitialMap();
-                }
-                else{
+                    System.out.println("These are your starter tiles!!!");
+                } else {
                     players.get(playersTurn).map.fillMapWithAllowedTilePlacements();
                     players.get(playersTurn).printMap(helperIntToPrintMap);
 
                     PairDisplay.showPairs();
-                    boolean continueAskingForInput=true;
-                    while(continueAskingForInput){
+                    boolean continueAskingForInput = true;
+                    while (continueAskingForInput) {
                         IOcascadia.instructionsToChoosePair();
-                        int instructionsToChoosePairInput=IOcascadia.takeIntInput() - 1;
-                        if(instructionsToChoosePairInput<=5 && instructionsToChoosePairInput>=0){
-                            if(instructionsToChoosePairInput<=3){
+                        int instructionsToChoosePairInput = IOcascadia.takeIntInput() - 1;
+                        if (instructionsToChoosePairInput <= 5 && instructionsToChoosePairInput >= 0) {
+                            if (instructionsToChoosePairInput <= 3) {
                                 players.get(playersTurn).pickPair(instructionsToChoosePairInput);
-                                continueAskingForInput=false;
-                            }
-                            else if(instructionsToChoosePairInput==4){
-                                if(players.get(playersTurn).getNatureTokenNumber()<=0){
+                                continueAskingForInput = false;
+                            } else if (instructionsToChoosePairInput == 4) {
+                                if (players.get(playersTurn).getNatureTokenNumber() <= 0) {
                                     System.out.println("You don't have nature tokens to use, try again");
-                                }
-                                else{
+                                } else {
                                     //cull again
                                     System.out.println("The river has been culled");
                                     TileDeck.cullRiver(4);
@@ -70,19 +68,16 @@ public class GameRunner {
                                     PairDisplay.showPairs();
                                 }
 
-                            }
-                            else {
-                                if(players.get(playersTurn).getNatureTokenNumber()<=0){
+                            } else {
+                                if (players.get(playersTurn).getNatureTokenNumber() <= 0) {
                                     System.out.println("You don't have nature tokens to use, try again");
-                                }
-                                else{
+                                } else {
                                     //pick one and one
                                     players.get(playersTurn).splitPick();
-                                    continueAskingForInput=false;
+                                    continueAskingForInput = false;
                                 }
                             }
-                        }
-                        else{
+                        } else {
                             System.out.println("Sorry, the input should be between 1 and 6, try again");
                         }
                     }
@@ -96,11 +91,10 @@ public class GameRunner {
                     System.out.println(token + "\n");
 
                     System.out.println("Would you like to rotate tiles(yes or no)");
-                    boolean wrongFlipTileCommand = true;
-                    while(wrongFlipTileCommand){
-                        String rotate=IOcascadia.makeLowerCase(IOcascadia.takeInput());
-                        if(rotate.equals("yes"))
-                        {
+                    boolean wrongInput = true;
+                    while (wrongInput) {
+                        String rotate = IOcascadia.makeLowerCase(IOcascadia.takeInput());
+                        if (rotate.equals("yes")) {
                             players.get(playersTurn).heldTile.flipTile(players.get(playersTurn).heldTile);
                             TileGenerator g = new TileGenerator(players.get(playersTurn).heldTile);
                             g.generateFlipTile();
@@ -109,16 +103,12 @@ public class GameRunner {
 
                             players.get(playersTurn).printMap(helperIntToPrintMap);
 
-                            wrongFlipTileCommand = false;
-                        }
-                        else if(rotate.equals("no"))
-                        {
-                            wrongFlipTileCommand = false;
+                            wrongInput = false;
+                        } else if (rotate.equals("no")) {
+                            wrongInput = false;
                             players.get(playersTurn).printMap(helperIntToPrintMap);
 
-                        }
-                        else
-                        {
+                        } else {
                             System.out.println("Wrong input please try again");
                         }
                     }
@@ -129,57 +119,54 @@ public class GameRunner {
                     int y = IOcascadia.takeIntInput();
                     //System.out.println(players.get(playersTurn).heldTile.getColour());
                     //System.out.println(players.get(playersTurn).heldTile.getColour2());
-                    players.get(playersTurn).placeTile(x,y);
+                    players.get(playersTurn).placeTile(x, y);
                     players.get(playersTurn).map.fillMapWithAllowedTilePlacements();
 
                     players.get(playersTurn).printMap(helperIntToPrintMap);
 
 
-                    if(players.get(playersTurn).checkToken())
-                    {
-                    }
-                    else
-                    {
+                    if (players.get(playersTurn).checkToken()) {
+                    } else {
                         System.out.println("Do you want to place the token? (yes or no)");
-                        String decision = IOcascadia.makeLowerCase(IOcascadia.takeInput());
+                        wrongInput = true;
+                        while (wrongInput) {
+                            String decision = IOcascadia.makeLowerCase(IOcascadia.takeInput());
 
-                        if (decision.equals("yes")) {
+                            if (decision.equals("yes")) {
 
-                            System.out.println("Where would you like to place token " + token);
-                            int coordinate = IOcascadia.takeIntInput();
-                            int coordinate2 = IOcascadia.takeIntInput();
-                            players.get(playersTurn).placeToken(coordinate, coordinate2);
-                            players.get(playersTurn).printMap(helperIntToPrintMap);
+                                System.out.println("Where would you like to place token " + token);
+                                int coordinate = IOcascadia.takeIntInput();
+                                int coordinate2 = IOcascadia.takeIntInput();
+                                players.get(playersTurn).placeToken(coordinate, coordinate2);
+                                players.get(playersTurn).printMap(helperIntToPrintMap);
+                                wrongInput = false;
 
 
-                        } else if (decision.equals("no")) {
-
+                            } else if (decision.equals("no")) {
+                                wrongInput = false;
+                            } else {
+                                System.out.println("expected a yes or no answer, please try again");
+                            }
                         }
                     }
 
                 }
 
-                boolean wrongCommand=true;
-                while(wrongCommand){
-                    System.out.println("Your turn has ended. What do you want to do, finish your turn (type next) or end the game (type quit)?");
-                    String decision=IOcascadia.makeLowerCase(IOcascadia.takeInput());
-
-                    switch((decision)) {
-                        case "next":
-                            wrongCommand=false;
-                            break;
-                        case "quit":
-                            continueGame=false;
-                            wrongCommand=false;
-                            break;
-                        default:
-                            System.out.println("You have entered an invalid command, try again");
-                            break;
+                boolean hasGameEnded = true;
+                while (hasGameEnded)
+                {
+                    System.out.println("Your turn has ended.");
+                    if (!hasGameEnded)
+                    {
+                        continueGame = false;
+                    } else
+                    {
+                        hasGameEnded = false;
                     }
+
+
+                    playersTurn++;
                 }
-
-
-                playersTurn++;
             }
         }
         System.out.println("Thanks for playing!");
